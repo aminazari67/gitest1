b5885d82d79b4e65e4d213e1b43142ee36a2dda0
melorin <aamin.azari@gmail.com>
2020-02-17 11:05:18 +0330

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    int i=0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
